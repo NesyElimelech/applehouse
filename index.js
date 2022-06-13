2e34197d57fed760478ebcbacaa660f1353662aa
NesyElimelech <nesy15@gmail.com>
2022-06-13 23:36:13 +0300

    Initial commit - full project

---
+++ index.js
@@ -0,0 +1,7 @@
+let loginForm = document.querySelector(".header .login-form");
+
+// Login form animation toggle switch.
+
+document.querySelector("#toggleActive").onclick = () => {
+  loginForm.classList.toggle("active");
+};
